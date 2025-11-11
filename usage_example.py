af6523d53dbf8100b3636cde1088b36734426a2c
Dani Gracia <danigraciaquiroga@gmail.com>
2025-11-11 12:08:42 +0100

    toml for poetry

--- usage_example.py
+++ usage_example.py
@@ -1,6 +1,6 @@
 from sudokugen import generateSudokus, sudokus2CSV
 
-sudokus = generateSudokus(2, 1)
+sudokus = generateSudokus(31, 0)
 
-csv_path = sudokus2CSV(sudokus, "2025-11-10")
+csv_path = sudokus2CSV(sudokus, "2025-12-01")
 print(csv_path)
